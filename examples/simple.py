6e3dfc04fcf8f20c36bf3f54bfc7e0de09b23be5
vcokltfre <vcokltfre@gmail.com>
2023-04-05 04:12:37 +0100

    chore: add bot functionality to example too

--- examples/simple.py
+++ examples/simple.py
@@ -1,5 +1,8 @@
 from os import environ
 
+from disnake import CommandInteraction
+from disnake.ext.commands import slash_command  # type: ignore
+
 from disnakeapi import Bot, Cog, Request
 
 
@@ -7,6 +10,10 @@ class ExampleCog(Cog):
     @Cog.get("/")
     async def index(self, request: Request) -> str:
         return "Hello, world!"
+
+    @slash_command(name="ping")
+    async def ping(self, itr: CommandInteraction) -> None:
+        await itr.send("Pong!")
 
 
 bot = Bot(environ["HOST"], int(environ["PORT"]))
